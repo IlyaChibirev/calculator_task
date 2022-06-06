98f8c4f6a232ef30ae821a6d51a9de2e925773de
IlyaChibirev <chibirevspartak@gamail.com>
2022-06-06 22:39:24 +0700

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Ints intsCalc = new IntsCalculator();
+        System.out.println(intsCalc.sum(7, 6));
+        System.out.println(intsCalc.mult(20, 13));
+        System.out.println(intsCalc.pow(3, 10));
+    }
+}
+
